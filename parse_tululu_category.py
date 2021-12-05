1c06ba11ddf96df9517545ae835c919851909145
konflic <konflic@mail.ru>
2021-12-05 12:52:30 +0300

    added arguments for category parser

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,11 +1,11 @@
-import re
+import argparse
 import requests
 
 from bs4 import BeautifulSoup
 from parse_tululu import save_books, check_for_redirect
 
 
-def get_category_html(category_id, page=1):
+def get_category_html(category_id="l55", page=1):
     page_prefix = f"{page}/" if page > 1 else ""
     response = requests.get(f"https://tululu.org/{category_id}/" + page_prefix, verify=False)
     response.raise_for_status()
@@ -19,13 +19,21 @@ def parse_category_page(page_html):
     soup = BeautifulSoup(page_html, "lxml")
     book_links = soup.select(selector="#content a[href^='/b']")
     for book_link in book_links:
-        book_ids.append(book_link["href"][2:-1])
+        book_ids.append(book_link["href"][2:-1]) # /b300/ -> b300
     return book_ids
 
 
 if __name__ == "__main__":
-    for page in range(1, 5):
-        category_html = get_category_html("l55", page=page)
-        book_ids = parse_category_page(category_html)
+    parser = argparse.ArgumentParser()
 
-        save_books(book_ids)
+    parser.add_argument("--start_page", type=int, default=1, required=False)
+    parser.add_argument("--end_page", type=int, default=5, required=False)
+
+    args = parser.parse_args()
+
+    book_ids = []
+    for page in range(args.start_page, args.end_page):
+        page_html = get_category_html(page=page)
+        book_ids.extend(parse_category_page(page_html))
+
+    save_books(book_ids)
