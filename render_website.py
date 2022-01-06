d068fbba39209593024440de0b0f3dadecd70517
konflic <konflic@mail.ru>
2022-01-06 18:32:50 +0300

    Add paginator

--- render_website.py
+++ render_website.py
@@ -2,6 +2,7 @@ import json
 import os
 import webbrowser
 
+from math import ceil
 from more_itertools import chunked
 from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -18,17 +19,19 @@ template = env.get_template("templates/template.html")
 def on_reload():
     with open("books_info.json", "r") as books_json:
         books = json.loads(books_json.read())
+        pages = range(1, ceil(len(books) / 20))
 
         for page, books in enumerate(chunked(books, 20), start=1):
             
             chunked_books = chunked(books, 2)
-            rendered_page = template.render(books=chunked_books)
+            rendered_page = template.render(books=chunked_books, pages=pages)
 
             os.makedirs("pages", exist_ok=True)
 
             with open(f"pages/index{page}.html", "w", encoding="utf-8") as file:
                 file.write(rendered_page)
 
+on_reload()
 
 server.watch("templates/*.html", on_reload)
 server.watch("books_info.json", on_reload)
