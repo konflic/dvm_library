76e4a9c726ebb0ed731a443d834cf6c4e8e9fd91
konflic <konflic@mail.ru>
2022-01-08 15:16:37 +0300

    Moved code from context managed

--- render_website.py
+++ render_website.py
@@ -17,23 +17,24 @@ env = Environment(
 template = env.get_template("templates/template.html")
 
 
-def on_reload():
-    with open("books_info.json", "r") as books_json:
+def on_reload(books_data_file="books_info.json"):
+    with open(books_data_file, "r") as books_json:
         books = json.load(books_json)
-        start_from = 1
-        books_in_row = 2
-        books_per_page = 20
-        pages = range(start_from, ceil(len(books) / books_per_page))
 
-        for page, books in enumerate(chunked(books, books_per_page), start=start_from):
-            
-            chunked_books = chunked(books, books_in_row)
-            rendered_page = template.render(books=chunked_books, pages=pages, current=page, prev=page-1, _next=page+1)
+    start_from = 1
+    books_in_row = 2
+    books_per_page = 20
+    pages = range(start_from, ceil(len(books) / books_per_page))
 
-            os.makedirs("pages", exist_ok=True)
+    for page, books in enumerate(chunked(books, books_per_page), start=start_from):
 
-            with open(f"pages/index{page}.html", "w", encoding="utf-8") as file:
-                file.write(rendered_page)
+        chunked_books = chunked(books, books_in_row)
+        rendered_page = template.render(books=chunked_books, pages=pages, current=page, prev=page-1, _next=page+1)
+
+        os.makedirs("pages", exist_ok=True)
+
+        with open(f"pages/index{page}.html", "w", encoding="utf-8") as file:
+            file.write(rendered_page)
 
 on_reload()
 
